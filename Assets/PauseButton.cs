298bcdd6430a09fd8a7df5c98cd6c0086f9ee809
Gorglomux <aralleaume@gmail.com>
2023-11-18 11:20:08 +0100

    Implemented pause button

---
+++ Assets/PauseButton.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.EventSystems;
+
+public class PauseButton : MonoBehaviour
+{
+    public void onClick()
+    {
+        if (GameManager.instance != null)
+        {
+            GameManager.instance.PauseGame();
+        }
+    }
+
+    //Shake the transform
+    public void onPointerEnter(BaseEventData data)
+    {
+        transform.DOShakeRotation(0.5f,new Vector3(0,0,5), 10, 15,true, ShakeRandomnessMode.Harmonic).SetEase(Ease.InBack);
+    }
+
+    //Reset to initialState
+    public void onPointerExit(BaseEventData data)
+    {
+        transform.DORotate(Vector3.zero, 0.1f);
+    }
+
+
+}
