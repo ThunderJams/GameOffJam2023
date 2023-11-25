2b7971e975e4435cbb543fee8b8df9eef31d6f2e
ThunderJams <57014565+ThunderJams@users.noreply.github.com>
2023-11-25 01:20:08 +0000

    Tutorial Cat Basic Implementation

---
+++ Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TutorialScript : MonoBehaviour
+{
+    float tutorialTimer = 0f;
+    bool tutorialDisplayed = false;
+
+    // array of bools for each tutorial
+    bool[] tutorialActivated = new bool[6];
+
+    string[] tutorialText = new string[6];
+
+    [SerializeField] Image professorSprite;
+
+    [SerializeField] List<GameObject> scales;
+    float initialHeight;
+
+    CatBase firstCat;
+
+    void Awake()
+    {
+        tutorialText[0] = "This is the cat tree! Cats love to climb on it, and the goal of the game is to keep it balanced! \n \n (click to continue)";
+        tutorialText[1] = "A cat has been launched from the cat cannon! Click and drag to move it onto the scale. \n \n (click to continue)";
+        tutorialText[2] = "Now that a cat has been placed on the scale, it leans towards the heavy side. \n \n (click to continue)";
+        tutorialText[3] = "The timer in the top left shows the length of the Round. Each round gets more difficult! \n \n (click to continue)";
+        tutorialText[4] = "The bar on the left is the Scratch-O-Meter! It increases for each cat that falls off-screen. \n \n (click to continue)";
+        tutorialText[5] = "Once it reaches it's full capacity, it's Game Over! Good luck! \n \n (click to continue)";
+        initialHeight = scales[0].transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tutorialTimer += Time.deltaTime;
+
+        
+
+        if (tutorialTimer > 1 && tutorialActivated[0] == false)
+        {
+            // new line
+            EnableTutorial(0);
+        }
+
+
+        if (firstCat){
+            if (firstCat.activated && tutorialActivated[1] == false)
+            {
+            // new line
+            EnableTutorial(1);
+            }
+        }
+        
+
+        if (tutorialActivated[2] == false){
+            foreach (GameObject scale in scales)
+            {
+                if (scale.transform.position.y < initialHeight - 0.2f)
+                {
+                    EnableTutorial(2);
+                }
+            }
+        }
+        else{
+            if (tutorialTimer > 20 && tutorialActivated[3] == false)
+            {
+                // new line
+                EnableTutorial(3);
+            }
+
+        }
+
+        if (tutorialTimer > 60 && tutorialActivated[4] == false)
+        {
+            // new line
+            EnableTutorial(4);
+        }
+        if (tutorialActivated[4] == true && tutorialActivated[5] == false && Time.timeScale == 1)
+        {
+            // new line
+            EnableTutorial(5);
+        }
+
+        // if click and tutorial is displayed
+        if (Input.GetMouseButtonDown(0) && tutorialDisplayed)
+        {
+            // disable tutorial
+            DisableTutorial();
+        }
+
+        if (firstCat == null)
+        {
+            firstCat = FindObjectOfType<CatBase>();
+        }
+    }
+
+    void EnableTutorial(int tutorialNumber){
+        tutorialActivated[tutorialNumber] = true;
+
+        tutorialDisplayed = true;
+        // freeze deltatime
+        Time.timeScale = 0;
+        // set self active
+        gameObject.GetComponent<Image>().enabled = true;
+        // set text
+        gameObject.GetComponentInChildren<TextMeshProUGUI>().enabled = true;
+        gameObject.GetComponentInChildren<TextMeshProUGUI>().text = tutorialText[tutorialNumber];
+
+        professorSprite.enabled = true;
+    }
+
+    void DisableTutorial(){
+        tutorialDisplayed = false;
+        // unfreeze deltatime
+        Time.timeScale = 1;
+        // set self inactive
+        gameObject.GetComponent<Image>().enabled = false;
+        gameObject.GetComponentInChildren<TextMeshProUGUI>().enabled = false;
+
+        professorSprite.enabled = false;
+    }
+}
